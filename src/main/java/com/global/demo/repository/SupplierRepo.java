7e72b0b5a60c5e38155aeb7e284570e4869ffa9a
abdullah <elgendyabdala@gmail.com>
2025-03-20 13:47:58 +0200

    delete package model

--- src/main/java/com/global/demo/repository/SupplierRepo.java
+++
@@ -1,9 +0,0 @@
-package com.global.demo.repository;
-
-import com.global.demo.entity.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SupplierRepo extends JpaRepository<Supplier,Long> {
-
-    Supplier getSuppliersById(Long id);
-}
