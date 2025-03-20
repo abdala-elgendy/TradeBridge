8f90e32c6af88db282d4b37d32d0c05f7f238082
abdullah <elgendyabdala@gmail.com>
2025-03-20 13:55:54 +0200

    reinitialize customer class

--- src/main/java/com/global/demo/entity/Customer.java
+++ src/main/java/com/global/demo/entity/Customer.java
@@ -2,32 +2,31 @@ package com.global.demo.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
 @Data
 @Entity
-@Setter
-@Getter
-@Table(name="customers")
-public class Customer {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
-    private String contactName;
-    private String address;
-    private String city;
-    private String phone;
-    private String email;
+@Table(name = "customers")
+@EqualsAndHashCode(callSuper = true)
+public class Customer extends BaseRoleEntity {
     
+    @Column(name = "shipping_address")
+    private String shippingAddress;
+    
+    @Column(name = "billing_address")
+    private String billingAddress;
+    
+    @Column(name = "preferred_payment_method")
+    private String preferredPaymentMethod;
+    
+    @Column(name = "loyalty_points")
+    private Integer loyaltyPoints = 0;
 
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
     private List<Order> orders;
 
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
     private List<Favorite> favorites;
-}
-
+} 
