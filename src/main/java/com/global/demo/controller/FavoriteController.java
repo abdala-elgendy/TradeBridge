29f6e3d747784dcbe221cbf964cee3faf402964a
abdullah <elgendyabdala@gmail.com>
2025-06-26 23:43:29 +0300

    feat : secure FavoriteController endpoints with Tokens for verification

--- src/main/java/com/global/demo/controller/FavoriteController.java
+++ src/main/java/com/global/demo/controller/FavoriteController.java
@@ -2,8 +2,10 @@
 package com.global.demo.controller;
 
 import com.global.demo.entity.Favorite;
+import com.global.demo.entity.User;
 import com.global.demo.service.FavoriteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,22 +18,22 @@ public class FavoriteController {
     private FavoriteService favoriteService;
 
     @PostMapping("/add")
-    public Favorite addFavorite(@RequestBody Favorite favorite) {
+    public Favorite addFavorite(@AuthenticationPrincipal User user, @RequestBody Favorite favorite) {
         return favoriteService.addFavorite(favorite);
     }
 
     @GetMapping("/allmyfavorites")
-    public List<Favorite> getAllFavorites() {
+    public List<Favorite> getAllFavorites(@AuthenticationPrincipal User user) {
         return favoriteService.getAllFavorites();
     }
 
     @GetMapping("/{id}")
-    public Favorite getFavoriteById(@PathVariable Long id) {
+    public Favorite getFavoriteById(@AuthenticationPrincipal User user, @PathVariable Long id) {
         return favoriteService.getFavoriteById(id);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteFavorite(@PathVariable Long id) {
+    public void deleteFavorite(@AuthenticationPrincipal User user, @PathVariable Long id) {
         favoriteService.deleteFavorite(id);
     }
 }
