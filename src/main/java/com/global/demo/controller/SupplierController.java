fd2b9cd9b0e4a7fbdf39f1677b1493ef8289824f
abdullah <elgendyabdala@gmail.com>
2025-06-26 23:44:35 +0300

    feat : secure SupplierController endpoints with Tokens for verification

--- src/main/java/com/global/demo/controller/SupplierController.java
+++ src/main/java/com/global/demo/controller/SupplierController.java
@@ -2,8 +2,10 @@
 package com.global.demo.controller;
 
 import com.global.demo.entity.Supplier;
+import com.global.demo.entity.User;
 import com.global.demo.service.SupplierService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,12 +18,12 @@ public class SupplierController {
     private SupplierService supplierService;
 
     @PostMapping("/add")
-    public Supplier addSupplier(@RequestBody Supplier supplier) {
+    public Supplier addSupplier(@AuthenticationPrincipal User user, @RequestBody Supplier supplier) {
         return supplierService.addSupplier(supplier);
     }
 
     @GetMapping("/allsuppliers")
-    public List<Supplier> getAllSuppliers() {
+    public List<Supplier> getAllSuppliers(@AuthenticationPrincipal User user) {
         return supplierService.getAllSuppliers();
     }
 
@@ -36,7 +38,7 @@ public class SupplierController {
     // }
 
     @DeleteMapping("/{id}")
-    public void deleteSupplier(@PathVariable Long id) {
+    public void deleteSupplier(@AuthenticationPrincipal User user, @PathVariable Long id) {
         supplierService.deleteSupplier(id);
     }
 }
