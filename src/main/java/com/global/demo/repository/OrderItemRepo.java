249e52922718bf5236e603e407ff70a9c6965e02
abdullah <elgendyabdala@gmail.com>
2025-07-22 00:07:26 +0300

    add mehtod findbyId in OrderItemRepo

--- src/main/java/com/global/demo/repository/OrderItemRepo.java
+++ src/main/java/com/global/demo/repository/OrderItemRepo.java
@@ -3,6 +3,10 @@ package com.global.demo.repository;
 import com.global.demo.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrderItemRepo extends JpaRepository<OrderItem, Long> {
+
+    Optional<OrderItem> findById(Long id);
 
 }
