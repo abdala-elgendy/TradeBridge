5436d98e33c9bb8931fd44935b3014fa775419ad
abdullah <elgendyabdala@gmail.com>
2025-08-02 22:17:16 +0300

    add : get each user with it's email with alone mehtod

--- src/main/java/com/global/demo/repository/UserRepository.java
+++ src/main/java/com/global/demo/repository/UserRepository.java
@@ -2,17 +2,32 @@ package com.global.demo.repository;
 
 import com.global.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    Optional<User> findByEmailForAuthentication(@Param("email") String email);
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.customer WHERE u.email = :email")
+    Optional<User> findByEmailWithCustomer(@Param("email") String email);
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.supplier WHERE u.email = :email")
+    Optional<User> findByEmailWithSupplier(@Param("email") String email);
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.shipper WHERE u.email = :email")
+    Optional<User> findByEmailWithShipper(@Param("email") String email);
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.admin WHERE u.email = :email")
+    Optional<User> findByEmailWithAdmin(@Param("email") String email);
+
 
     boolean existsByEmail(String email);
 
     boolean existsByNationalId(String nationalId);
-
-    Optional<User> findByVerificationToken(String token);
 }
