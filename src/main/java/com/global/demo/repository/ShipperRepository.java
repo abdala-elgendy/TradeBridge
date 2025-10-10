d323f425488d44c14d8f348bf2a194cf0543e7df
abdullah <elgendyabdala@gmail.com>
2025-10-10 18:18:05 +0300

    fix  [shipperRepo] : solve fetching unrelated data from database

--- src/main/java/com/global/demo/repository/ShipperRepository.java
+++ src/main/java/com/global/demo/repository/ShipperRepository.java
@@ -2,8 +2,18 @@ package com.global.demo.repository;
 
 import com.global.demo.entity.Shipper;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ShipperRepository extends JpaRepository<Shipper, Long> {
+    
+    @Query("SELECT s FROM Shipper s JOIN FETCH s.user WHERE s.user.id = :userId")
+    Optional<Shipper> findByUserId(@Param("userId") Long userId);
+    
+    @Query("SELECT s FROM Shipper s JOIN FETCH s.user WHERE s.user.email = :email")
+    Optional<Shipper> findByUserEmail(@Param("email") String email);
 }
