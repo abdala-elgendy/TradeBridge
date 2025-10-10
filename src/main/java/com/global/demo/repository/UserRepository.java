5bebfd31bba033d516a4ff6112042a68996bb8eb
abdullah <elgendyabdala@gmail.com>
2025-10-10 18:18:30 +0300

    fix  [UserRepository] : solve fetching unrelated data from database

--- src/main/java/com/global/demo/repository/UserRepository.java
+++ src/main/java/com/global/demo/repository/UserRepository.java
@@ -2,17 +2,22 @@ package com.global.demo.repository;
 
 import com.global.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+    
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.customer LEFT JOIN FETCH u.supplier LEFT JOIN FETCH u.shipper LEFT JOIN FETCH u.admin WHERE u.email = :email")
+    Optional<User> findByEmail(@Param("email") String email);
 
     boolean existsByEmail(String email);
 
     boolean existsByNationalId(String nationalId);
 
-    Optional<User> findByVerificationToken(String token);
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.customer LEFT JOIN FETCH u.supplier LEFT JOIN FETCH u.shipper LEFT JOIN FETCH u.admin WHERE u.verificationToken = :token")
+    Optional<User> findByVerificationToken(@Param("token") String token);
 }
